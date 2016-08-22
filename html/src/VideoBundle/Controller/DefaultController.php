7afa16c50fc0c51e2d543c0e2eba25e36e63f2d7
rolandkasa95 <rolandkasa95@gmail.com>
2016-08-22 15:41:38 +0300

    New: controller action for inserting a new video by url

--- html/src/VideoBundle/Controller/DefaultController.php
+++ html/src/VideoBundle/Controller/DefaultController.php
@@ -1,10 +1,14 @@
 <?php
 
-namespace src\VideoBundle\Controller;
+namespace VideoBundle\Controller;
 
 use Doctrine\ORM\EntityManager;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use VideoBundle\Entity\Post;
 
 class DefaultController extends Controller
 {
@@ -22,5 +26,44 @@ class DefaultController extends Controller
         $videos = $repository->findAll();
 
         return $this->render('VideoBundle:Default:index.html.twig', array('videos' => $videos));
+    }
+
+    /**
+     * Matches /VideoAddition/*
+     *
+     * @Route("/VideoAddition",name="videoAddition")
+     *
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function newAction(Request $request)
+    {
+        // create a task and give it some dummy data for this example
+        $newVideo = new Post();
+        $newVideo->setVideoUrl('https://www.youtube.com/watch?v=CS-85i409PM');
+
+        $form = $this->createFormBuilder($newVideo)
+            ->add('VideoUrl', TextType::class, array('label' => "New Video Url: "))
+            ->add('save', SubmitType::class, array('label' => 'Add new Video'))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $newVideo = $form->getData();
+            $newVideo->setVideoUrl(str_replace("watch?v=","embed/",$newVideo->getVideoUrl()));
+
+
+            $manager = $this->getDoctrine()->getManager();
+
+            $manager->persist($newVideo);
+            $manager->flush();
+
+            return $this->redirectToRoute('video_homepage');
+        }
+
+        return $this->render('VideoBundle:Default:new.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 }
