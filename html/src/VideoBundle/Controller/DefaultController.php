5a5ec91ccd0edecd4e828aa0550dc77e6cff5da6
rolandkasa95 <rolandkasa95@gmail.com>
2016-08-22 15:58:48 +0300

    Update: refactor the function newAction in DefaultController

--- html/src/VideoBundle/Controller/DefaultController.php
+++ html/src/VideoBundle/Controller/DefaultController.php
@@ -2,10 +2,10 @@
 
 namespace VideoBundle\Controller;
 
-use Doctrine\ORM\EntityManager;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Form;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use VideoBundle\Entity\Post;
@@ -43,12 +43,25 @@ class DefaultController extends Controller
         $newVideo->setVideoUrl('https://www.youtube.com/watch?v=CS-85i409PM');
 
         $form = $this->createFormBuilder($newVideo)
-            ->add('VideoUrl', TextType::class, array('label' => "New Video Url: "))
+            ->add('VideoUrl', UrlType::class, array('label' => "New Video Url: "))
             ->add('save', SubmitType::class, array('label' => 'Add new Video'))
             ->getForm();
 
         $form->handleRequest($request);
 
+        $this->submitData($form);
+
+        return $this->render('VideoBundle:Default:new.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * @param Form $form
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    private function submitData($form)
+    {
         if ($form->isSubmitted() && $form->isValid()) {
             $newVideo = $form->getData();
             $newVideo->setVideoUrl(str_replace("watch?v=","embed/",$newVideo->getVideoUrl()));
@@ -61,9 +74,7 @@ class DefaultController extends Controller
 
             return $this->redirectToRoute('video_homepage');
         }
-
-        return $this->render('VideoBundle:Default:new.html.twig', array(
-            'form' => $form->createView(),
-        ));
     }
 }
+
+
